d0c8b43031546a5cfddf0709c423f68dc4cbd7ee
Leonardo Weiss <lsweissleo@gmail.com>
2024-03-15 22:06:02 -0300

    adding checkbox to mark done exercises

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,20 +1,18 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import '@fortawesome/fontawesome-free/css/all.css'
 import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata = {
   title: "TRAINING APP",
-  description: "App to Gym Lovers",
+  description: "App to Gym Lovers"
 };
 
 export default function RootLayout({ children }) {
+
   return (
     <html lang="pt-br">
-      <body className={inter.className}>
+      <body className='dark'>
         <Header />
         {children}
         <Footer />
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,17 +2,22 @@ function List({ exercises }) {
     return(
         <section>
             {exercises.map(ex => (
-                <a href={ex.link}>
-                    <div className="box">
-                        <h3>
-                            {ex.name}
-                        </h3>
-                        <div className="infos">
-                            <div>Séries: <strong>{ex.series}</strong></div>
-                            <div>Repetições: <strong>8 a 12</strong></div>
-                        </div>
+                <div id="exercise">
+                    <div id="mark">
+                        <input type="checkbox" name="check" id="check"/>
                     </div>
-                </a>
+                    <a href={ex.link}>
+                        <div className="box">
+                            <h3>
+                                {ex.name}
+                            </h3>
+                            <div className="infos">
+                                <div>Séries: <strong>{ex.series}</strong></div>
+                                <div>Repetições: <strong>8 a 12</strong></div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
             ))}
         </section>
     )
